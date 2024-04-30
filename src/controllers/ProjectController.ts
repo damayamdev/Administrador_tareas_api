7217bbc99d7c490053b0df69e541c8ef260776df
damayamdev <164294520+damayamdev@users.noreply.github.com>
2024-04-30 10:38:49 -0500

    Se agrega el return

--- src/controllers/ProjectController.ts
+++ src/controllers/ProjectController.ts
@@ -8,17 +8,17 @@ export class ProjectController {
         const project = new Project(req.body)
         try {
             await project.save()
-            HttpResponse.OK(res, "Proyecto creado correctamente")
+            return HttpResponse.OK(res, "Proyecto creado correctamente")
         } catch (error) {
-            HttpResponse.Error(res, error)
+            return HttpResponse.Error(res, error)
         }
     }
     static getAllProjects = async (req: Request, res: Response) => {
         try {
             const projects = await Project.find({})
-            HttpResponse.OK(res, projects)
+            return HttpResponse.OK(res, projects)
         } catch (error) {
-            HttpResponse.Error(res, error)
+            return HttpResponse.Error(res, error)
         }
     }
     static getProjectById = async (req: Request, res: Response) => {
@@ -28,9 +28,9 @@ export class ProjectController {
             if (!project) {
                 return HttpResponse.NotFound(res, 'Proyecto no Encontrado en la Base de Datos')
             }
-            HttpResponse.OK(res, project)
+            return HttpResponse.OK(res, project)
         } catch (error) {
-            HttpResponse.Error(res, error)
+            return HttpResponse.Error(res, error)
         } 
     }
     static updateProjectById = async (req: Request, res: Response) => {
@@ -47,10 +47,10 @@ export class ProjectController {
             project.description = req.body.description
             
             await project.save()
-            HttpResponse.OKPERSONALIZADO(res, 'Proyecto Actualizado Correctamente')
+            return HttpResponse.OKPersonalizado(res, 'Proyecto Actualizado Correctamente')
 
         } catch (error) {
-            HttpResponse.Error(res, error)
+            return HttpResponse.Error(res, error)
         } 
     }
     static deleteProjectById = async (req: Request, res: Response) => {
@@ -61,10 +61,10 @@ export class ProjectController {
                 return HttpResponse.NotFound(res, 'Proyecto no Encontrado en la Base de Datos')
             }
             await project.deleteOne()
-            HttpResponse.OKPERSONALIZADO(res, 'Proyecto Eliminado Correctamente')
+            return HttpResponse.OKPersonalizado(res, 'Proyecto Eliminado Correctamente')
 
         } catch (error) {
-            HttpResponse.Error(res, error)
+            return HttpResponse.Error(res, error)
         } 
     }
 }
--- src/controllers/TaskController.ts
+++ src/controllers/TaskController.ts
@@ -10,10 +10,9 @@ export class TaskController {
             task.project = req.project.id
             req.project.tasks.push(task.id)
             await Promise.allSettled([task.save(), req.project.save()])
-            HttpResponse.OKPERSONALIZADO(res, "Tarea creada correctamente")
+            return HttpResponse.OKPersonalizado(res, "Tarea creada correctamente")
         } catch (error) {
-            console.log(error)
-            HttpResponse.Error(res, error)
+            return HttpResponse.Error(res, error)
         }
     }
 
@@ -23,19 +22,17 @@ export class TaskController {
             if (!task) {
                 return HttpResponse.NotFound(res, 'Tarea no Encontrada en la Base de Datos')
             }
-            HttpResponse.OK(res, task)
+            return HttpResponse.OK(res, task)
         } catch (error) {
-            console.log(error)
-            HttpResponse.Error(res, error)
+            return HttpResponse.Error(res, error)
         }
     }
 
     static getTaskById = async (req: Request, res: Response) => {
         try {
-            HttpResponse.OK(res, req.task)
+            return HttpResponse.OK(res, req.task)
         } catch (error) {
-            console.log(error)
-            HttpResponse.Error(res, error)
+            return HttpResponse.Error(res, error)
         }
     }
 
@@ -44,10 +41,9 @@ export class TaskController {
             req.task.name = req.body.name
             req.task.description = req.body.description
             await req.task.save()
-            HttpResponse.OKPERSONALIZADO(res, 'Tarea Actualizada Correctamente')
+            return HttpResponse.OKPersonalizado(res, 'Tarea Actualizada Correctamente')
         } catch (error) {
-            console.log(error)
-            HttpResponse.Error(res, error)
+            return HttpResponse.Error(res, error)
         }
     }
 
@@ -56,10 +52,9 @@ export class TaskController {
             req.project.tasks = req.project.tasks.filter(task => task.toString() !== req.task.id.toString())
             await Promise.allSettled([req.task.deleteOne(), req.project.save()])
 
-            HttpResponse.OKPERSONALIZADO(res, 'Tarea Eliminada Correctamente')
+            return HttpResponse.OKPersonalizado(res, 'Tarea Eliminada Correctamente')
         } catch (error) {
-            console.log(error)
-            HttpResponse.Error(res, error)
+            return HttpResponse.Error(res, error)
         }
     }
 
@@ -68,10 +63,9 @@ export class TaskController {
             const {status} = req.body
             req.task.status = status
             await req.task.save()
-            HttpResponse.OKPERSONALIZADO(res, 'Estado Actualizado')
+            return HttpResponse.OKPersonalizado(res, 'Estado Actualizado')
         } catch (error) {
-            console.log(error)
-            HttpResponse.Error(res, error)
+            return HttpResponse.Error(res, error)
         }
     }
 }
