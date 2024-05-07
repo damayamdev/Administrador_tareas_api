08ac84d04609331450b6440837250c3d332b80e9
damayamdev <164294520+damayamdev@users.noreply.github.com>
2024-05-07 08:34:51 -0500

    cors

--- src/config/cors.ts
+++ src/config/cors.ts
@@ -2,6 +2,7 @@ import {CorsOptions} from 'cors'
 
 export const corsConfig: CorsOptions = {
     origin: function(origin, callback) {
+        console.log(origin)
         const whitelist = [process.env.FRONTEND_URL]
         if (process.argv[2] === '--api') {
             whitelist.push(undefined)
